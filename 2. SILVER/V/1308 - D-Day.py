6512b18df533bbf0dc91a01dd590fa3174cc551d
illlilililiililll <kcwrenew22c@gmail.com>
2024-03-26 22:03:17 +0900

    1308 - D-Day

---
+++ 2. SILVER/V/1308 - D-Day.py
@@ -0,0 +1,12 @@
+import datetime
+
+y, m, d = map(int, input().split())
+yx, mx, dx = map(int, input().split())
+now = datetime.date(y, m, d)
+exit = datetime.date(y+1000, m, d)
+end = datetime.date(yx, mx, dx)
+
+if end > exit:
+    print('gg')
+else:
+    print(f"D-{(end-now).days}")
