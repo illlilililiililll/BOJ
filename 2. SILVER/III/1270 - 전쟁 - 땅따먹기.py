096c2009b3c8f4ed24219b4fcff357808feed34f
illlilililiililll <kcwrenew22c@gmail.com>
2024-04-06 19:37:49 +0900

    20240406

--- 2. SILVER/III/1270 - 전쟁 - 땅따먹기.py
+++ 2. SILVER/III/1270 - 전쟁 - 땅따먹기.py
@@ -6,9 +6,9 @@ for _ in range(int(input())):
     T = list(map(int, input().split()))
     n = T.pop(0)
 
-    T = collections.Counter(T).most_common()
+    T = collections.Counter(T).most_common(1)[0]
     
-    if T[0][1]/n > 0.5:
-        print(T[0][0])
+    if T[1]/n > 0.5:
+        print(T[0])
     else:
         print("SYJKGW")
